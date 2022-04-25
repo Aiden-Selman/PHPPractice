de14fcad331e12da053ef6116b3c5de5cfbc9dac
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-04-25 18:22:42 -0500

    Update PHPPractice.php
    Added simple numeric variables and addition

--- PHPPractice.php
+++ PHPPractice.php
@@ -1,4 +1,10 @@
 <?php
-$txt = "PHP";
-echo "I love $txt";
+
+    // simple text output
+    $txt = "PHP";
+    echo "I love $txt";
+    // numeric variables
+    $x = 5;
+    $y = 4;
+    echo $x + $y;
 ?>
