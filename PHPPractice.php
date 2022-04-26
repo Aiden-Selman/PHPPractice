a02fd3189602595310bc8e0057fde9c3ba57c1f2
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-04-26 15:23:32 -0500

    Update PHPPractice.php
    Added variables in different scopes and tested them

--- PHPPractice.php
+++ PHPPractice.php
@@ -4,7 +4,29 @@
     $txt = "PHP";
     echo "I love $txt";
     // numeric variables
-    $x = 5;
+    //$x = 5;
     $y = 4;
     echo $x + $y;
+    // scope
+    $x = 5; // global scope
+    // this will not work
+    function myTest()
+    {
+    // using x inside this function will generate an error
+    echo "<p>Variable x inside function is: $x</p>";
+    }
+    myTest();
+
+    echo "<p>Variable x outside function is: $x</p>";
+
+    //this will work
+    function myTest2() 
+    {
+        $x = 5; // local scope
+        echo "<p>Variable x inside function is: $x</p>";
+    }
+    myTest();
+      
+    // using x outside the function will generate an error
+    echo "<p>Variable x outside function is: $x</p>";
 ?>
