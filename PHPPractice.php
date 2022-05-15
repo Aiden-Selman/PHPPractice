9cb9fe767319d70c7874371d572bfc66e35719d9
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-14 20:43:22 -0500

    Update PHPPractice.php
    Added an example of the strpos string function

--- PHPPractice.php
+++ PHPPractice.php
@@ -75,5 +75,6 @@
     // String functions
     echo strlen("Hello World!"); // outputs 12
     echo str_word_count("Hello world!"); // outputs 2
-    echo strrev("Hello world!") // outputs !dlrow olleH
+    echo strrev("Hello world!"); // outputs !dlrow olleH
+    echo strpos("Hello world!", "world"); // outputs 6
 ?>
