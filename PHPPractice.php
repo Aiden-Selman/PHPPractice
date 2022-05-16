4468d70fed293e4e178310c948f5a028bdd4a561
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-05-15 19:32:16 -0500

    Update PHPPractice.php
    Added an example of the string replace function

--- PHPPractice.php
+++ PHPPractice.php
@@ -77,4 +77,5 @@
     echo str_word_count("Hello world!"); // outputs 2
     echo strrev("Hello world!"); // outputs !dlrow olleH
     echo strpos("Hello world!", "world"); // outputs 6
+    echo str_replace("world", "Dolly", "Hello world!"); // outputs Hello Dolly!
 ?>
