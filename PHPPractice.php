2db13b337823e0b6438cc918be9083bb88ce5bc9
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-06-23 20:02:43 -0500

    Update PHPPractice.php
    Added an example of a switch statement

--- PHPPractice.php
+++ PHPPractice.php
@@ -151,4 +151,22 @@
     {
     echo "Have a good night!";
     }
+
+    // Switch statement
+    $favcolor = "red";
+
+    switch ($favcolor) 
+    {
+        case "red":
+            echo "Your favorite color is red!";
+            break;
+        case "blue":
+            echo "Your favorite color is blue!";
+            break;
+        case "green":
+            echo "Your favorite color is green!";
+            break;
+        default:
+            echo "Your favorite color is neither red, blue, nor green!";
+    }
 ?>
