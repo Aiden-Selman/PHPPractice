ecc9c15c12c30b7fdc6e21c3687e049825e941d2
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-06-28 20:58:42 -0500

    Update PHPPractice.php
    Added an example of a break continue loop

--- PHPPractice.php
+++ PHPPractice.php
@@ -202,4 +202,13 @@
     {
         echo "$value <br>";
     }
+
+    // Break/continue loop
+    for ($x = 0; $x < 10; $x++) 
+    {
+        if ($x == 4) {
+          break;
+        }
+        echo "The number is: $x <br>";
+    }
 ?>
