fdf8d415163a474bd4182bf1b565f6d1ddfe25db
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-07-02 19:21:06 -0500

    Update PHPPractice.php
    Added examples of functions with strict variable types

--- PHPPractice.php
+++ PHPPractice.php
@@ -244,4 +244,21 @@
     familyName("Hege", "1975");
     familyName("Stale", "1978");
     familyName("Kai Jim", "1983");
+
+    // Strict variable function example
+    function addNumbers(int $a, int $b) 
+    {
+        return $a + $b;
+    }
+    echo addNumbers(5, "5 days");
+    // since strict is NOT enabled "5 days" is changed to int(5), and it will return 10
+
+    declare(strict_types=1); // strict requirement
+
+    function addNumbers(int $a, int $b) 
+    {
+        return $a + $b;
+    }
+    echo addNumbers(5, "5 days");
+    // since strict is enabled and "5 days" is not an integer, an error will be thrown
 ?>
