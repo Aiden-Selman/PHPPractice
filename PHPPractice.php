545f6a08504b3721c6f3b3607db949e614374778
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-07-06 22:41:18 -0500

    Update PHPPractice.php
    Added an example of how to loop through an array

--- PHPPractice.php
+++ PHPPractice.php
@@ -288,4 +288,14 @@
     // Declaration
     $cars = array("Volvo", "BMW", "Toyota");
     echo "I like " . $cars[0] . ", " . $cars[1] . " and " . $cars[2] . ".";
+
+    // Looping through an array
+    $cars = array("Volvo", "BMW", "Toyota");
+    $arrlength = count($cars);
+
+    for($x = 0; $x < $arrlength; $x++) 
+    {
+        echo $cars[$x];
+        echo "<br>";
+    }
 ?>
