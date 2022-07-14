a2e5c33334551d5cbaae5f056f9dddbdfe4ddff2
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-07-13 22:43:20 -0500

    Update PHPPractice.php
    Added an example of how to loop through a two dimensional array

--- PHPPractice.php
+++ PHPPractice.php
@@ -321,4 +321,16 @@
         array("Saab",5,2),
         array("Land Rover",17,15)
     );
+
+    // Looping through a two dimensional array
+    for ($row = 0; $row < 4; $row++) 
+    {
+        echo "<p><b>Row number $row</b></p>";
+        echo "<ul>";
+        for ($col = 0; $col < 3; $col++) 
+        {
+          echo "<li>".$cars[$row][$col]."</li>";
+        }
+        echo "</ul>";
+    }
 ?>
