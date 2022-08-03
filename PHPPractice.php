207fb1e9459e6ebeadb32ea62498d1eb73490ee9
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-02 21:58:52 -0500

    Update PHPPractice.php
    Added an example of globals and how to use them

--- PHPPractice.php
+++ PHPPractice.php
@@ -370,8 +370,28 @@
     $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
     arsort($age);
 
-    // ksort
+    // krsort
     // The following example sorts an associative array in descending order, according to the key
     $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
     krsort($age);
+
+    // Superglobals
+    /* 
+        $GLOBALS is a PHP super global variable which is used to access global variables from anywhere in the PHP script (also from within functions or methods).
+
+        PHP stores all global variables in an array called $GLOBALS[index]. The index holds the name of the variable.
+
+        The example below shows how to use the super global variable $GLOBALS
+    */
+
+    $x = 75;
+    $y = 25;
+    
+    function addition() 
+    {
+        $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y'];
+    }
+    
+    addition();
+    echo $z;
 ?>
