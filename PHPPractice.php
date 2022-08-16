737c0f3ec75ed701a0910197a18a1e3cb5bceb81
Electraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-08-15 21:56:25 -0500

    Update PHPPractice.php
    Added an example of the $_GET superglobal

--- PHPPractice.php
+++ PHPPractice.php
@@ -457,4 +457,10 @@
           echo $name;
         }
     }
+
+    // $_GET
+    /*
+        PHP $_GET is a PHP super global variable which is used to collect form data after submitting an HTML form with method="get"
+    */
+    echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
 ?>
